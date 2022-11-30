cdb4c09fa129786ae73e5e5bab83eb47d29bda31
benjiesinzore <benjaminsinzore@gmail.com>
2022-11-30 18:32:25 +0300

    Add all the dependencies required by the project.

--- app/src/main/java/com/benjaminsinzore/equitym_bankingclone/MainActivity.kt
+++ app/src/main/java/com/benjaminsinzore/equitym_bankingclone/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -15,14 +17,17 @@ import com.google.accompanist.adaptive.calculateDisplayFeatures
 
 
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             EquityMBankingCloneTheme {
 
-//                val windowSize = calculateWindowSizeClass(this)
+                val windowSize = calculateWindowSizeClass(this)
                 val displayFeatures = calculateDisplayFeatures(this)
 //                val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
